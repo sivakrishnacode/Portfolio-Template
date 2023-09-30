04726d41b1e4e7e96ecf8c7bb7c980b0933e4c9b
sivacartrabbit <siva@cartrabbit.in>
2023-09-30 21:18:13 +0530

    resume link

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -102,7 +102,7 @@ const Experience = () => {
               ease-in-out"
               onClick={() =>
                 window.open(
-                  'resume link', //paste the link to your resume here
+                  'https://drive.google.com/file/d/1o0EeiIHRPHWt1ko6WsPPj6Vxov9aWOJg/view?usp=drive_link', //paste the link to your resume here
                   '_blank'
                 )
               }
